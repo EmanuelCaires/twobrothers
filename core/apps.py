20d5ef7632ffb1e958d1ed6487bf8164f4bf688c
emanuel caires <emanuelcaires1@gmail.com>
2025-01-06 11:42:21 +0000

    `Moved UserProfile import and receiver to ready method in CoreConfig`

--- core/apps.py
+++ core/apps.py
@@ -1,18 +1,16 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from .models import UserProfile
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
     def ready(self):
+        from .models import UserProfile  # Moved import here
         User = get_user_model()
-        post_save.connect(create_user_profile, sender=User)
+        post_save.connect(self.create_user_profile, sender=User)
 
-@receiver(post_save, sender=get_user_model())
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+    def create_user_profile(self, sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
