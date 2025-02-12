83820b59471eb1c0358c958069ab828e48d34335
emanuel caires <emanuelcaires1@gmail.com>
2025-02-12 17:33:06 +0000

    `Update DJANGO_SETTINGS_MODULE to production in wsgi.py`

--- werepair/wsgi.py
+++ werepair/wsgi.py
@@ -1,6 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'werepair.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'werepair.settings.production')
+
 
 application = get_wsgi_application()
